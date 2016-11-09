4c7c62b868d7692f41e1f26e5733ef7f1aac7ce2
Akiko Okabe <aki0u0@gmail.com>
2016-11-09 12:19:49 -0700

    add error handler to controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,9 @@
 class AnimalsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  def record_not_found
+    render json: {error: 'not found'}, status: 404
+  end
 
   def index
     @animals = Animal.all

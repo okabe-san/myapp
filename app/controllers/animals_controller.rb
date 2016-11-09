5980fc42ee0b78e2bef4ec60ec38181c4aa3039c
Akiko Okabe <aki0u0@gmail.com>
2016-11-09 11:01:38 -0700

    All base controllers (routes) updated for front end

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -14,21 +14,19 @@ class AnimalsController < ApplicationController
     @animal = Animal.create(animal_params)
   end
 
-  private
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update_attributes(animal_params)
+  end
 
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+  end
+
+  private
   def animal_params
     params.require(:animal).permit(:name, :age, :gender, :description, :url)
   end
-  # def edit
-  #   @animal = Animal.find(params[:id])
-  # end
-  #
-  # def update
-  #   params.permit!
-  #   @animal = Animal.update(params[:id])
-  # end
-  #
-  # def destroy
-  # end
 
 end
